1539181fea0c305a5335f7f284c59ccfb0e8b59d
jafarbeksayfiddinov <jafarbeksayfiddinov@gmail.com>
2025-04-11 09:15:07 +0500

    1st commit

---
+++ app/src/main/java/com/example/physicsschoolapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.physicsschoolapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        FirebaseDatabase instance = FirebaseDatabase.getInstance();
+        DatabaseReference message = instance.getReference("message");
+        message.setValue("Hello from Android Studio!");
+    }
+}
